09662fa101b737b6beca746a68ba15a2e0e6d799
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2023-12-22 19:39:23 -0300

    Created project

---
+++ src/main/java/com/example/fortuno_api/FortunoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fortuno_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortunoApiApplication.class, args);
+	}
+
+}

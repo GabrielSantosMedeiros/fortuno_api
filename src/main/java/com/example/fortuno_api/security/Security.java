e545815893bb57da34ab1de679f6b23a6e430875
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2023-12-23 10:32:27 -0300

    Configured user model  as principal in security class

--- src/main/java/com/example/fortuno_api/security/Security.java
+++ src/main/java/com/example/fortuno_api/security/Security.java
@@ -1,15 +1,24 @@
 package com.example.fortuno_api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.fortuno_api.services.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class Security {
     
+    @Autowired
+    UserService userService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -17,5 +26,15 @@ public class Security {
                 .anyRequest().permitAll()    
             )
             .build();
+    }
+
+    @Bean
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
